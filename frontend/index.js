6f4040666e7ab214bc7d9c8b7e957b1d2fab7c50
Paul Mougel <mougelpaul@gmail.com>
2014-01-15 22:03:05 +0100

    Added RSSI shim

--- frontend/index.js
+++ frontend/index.js
@@ -15,7 +15,13 @@ app.get('/smoothie.js', function (req, res) {
 var csvStream = csv();
 csvStream.transform(function (row) {
 	if (row[0] === 'temperature') {
-		io.sockets.emit('temperature', parseInt(row[1]));
+		io.sockets.emit('temperature', parseFloat(row[1]));
+	}
+	else if (row[0] === 'rssi') {
+		io.sockets.emit('rssi', {
+			nodeId: parseInt(row[1]),
+			rssi: parseFloat(row[2])
+		});
 	}
 	return null;
 });
@@ -25,7 +31,22 @@ if (process.env.NODE_ENV != 'test') {
 } else { // shim data
 	var inputStream = new stream.PassThrough();
 	inputStream.pipe(csvStream);
+
+	function getRandomArbitary (min, max) {
+		return Math.random() * (max - min) + min;
+	}
+
+	function getRandomInt (min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	// Format: temperature, tempInDegrees
 	setInterval(function () {
-		inputStream.write('temperature,' + Math.random() * 30 + '\n'); // randrom temp. up to 30deg.
+		inputStream.write('temperature,' + getRandomArbitary(10, 30) + '\n');
+	}, 1000);
+
+	// Format: rssi, nodeId, dbi
+	setInterval(function () {
+		inputStream.write('rssi,' + getRandomInt(0, 50) + ',' + getRandomArbitary(-100, -30) + '\n');
 	}, 1000);
 }
